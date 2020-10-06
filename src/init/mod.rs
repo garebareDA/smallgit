48c9f14d99a3005648a82186c81e45b62d77cb20
garebare <garedaiki@outlook.jp>
2020-10-06 15:09:19 +0900

    fix

--- src/init/mod.rs
+++ src/init/mod.rs
@@ -1,11 +1,18 @@
 pub mod init_create;
 
-use std::fs;
-#[test]
-fn init() {
-  match init_create::create_init_file() {
-    Ok(_) => {}
-    Err(e) => panic!(e),
+#[cfg(test)]
+mod test {
+  use std::fs;
+  use super::init_create;
+
+  #[test]
+  fn init() {
+    match init_create::create_init_file() {
+      Ok(_) => {}
+      Err(e) => panic!(e),
+    }
+    fs::remove_dir_all("./.smallgit")
+      .ok()
+      .expect("remove error");
   }
-  fs::remove_dir_all("./.smallgit");
 }
