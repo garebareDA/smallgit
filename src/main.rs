8f589f617c3a5b3cf8d3ec0bdc660258c2463b75
garebare <garedaiki@outlook.jp>
2020-10-06 14:00:30 +0900

    add:init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use std::env;
+use std::fs;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args[1] == "init" {
+        match create_init_file() {
+            Err(e) => eprintln!("{}", e),
+            Ok(_) => println!("init complete"),
+        }
+        return;
+    }
+
+    if args[2] == "add" {
+        
+    }
+}
+
+fn create_init_file() -> Result<(), String> {
+    match fs::create_dir("./.smallgit") {
+        Ok(_) => {}
+        Err(_) => {
+            return Err("failed to create dir".to_string());
+        }
+    }
+
+    match File::create("./.smallgit/index") {
+        Ok(_) => {}
+        Err(_) => {
+            return Err("failed to create  file".to_string());
+        }
+    }
+
+    match fs::create_dir("./.smallgit/refs") {
+        Ok(_) => {}
+        Err(_) => {
+            return Err("failed to create dir".to_string());
+        }
+    }
+
+    match fs::create_dir("./.smallgit/objects") {
+        Ok(_) => {}
+        Err(_) => {
+            return Err("failed to create dir".to_string());
+        }
+    }
+
+    return Ok(());
 }
