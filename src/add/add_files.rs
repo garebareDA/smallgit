22f45a91dcd208e1ea4bca5572c741babf26897d
garebare <garedaiki@outlook.jp>
2020-10-09 22:40:20 +0900

    add:write_index

--- src/add/add_files.rs
+++ src/add/add_files.rs
@@ -1,6 +1,10 @@
+use flate2::write::ZlibEncoder;
+use flate2::Compression;
+use crypto::digest::Digest;
+use crypto::sha1::Sha1;
 use std::fs;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
 use std::path::Path;
 
 pub struct AddFile {
@@ -33,6 +37,13 @@ impl AddFile {
     };
 
     match self.write_index() {
+      Ok(_) => {}
+      Err(s) => {
+        return Err(s);
+      }
+    }
+    //とりあえず objectsに書きだし
+    match self.create_objects() {
       Ok(_) => {}
       Err(s) => {
         return Err(s);
@@ -122,12 +133,61 @@ impl AddFile {
   }
 
   fn write_index(&self) -> Result<(), String> {
-    let index_path = "./.smallgit/index";
-    let objects_path = "./.smallgit/objects";
-    //とりあえず objectsに書きだし
     //indexステータスをなしでに書き込み
     //まだ前回のコミットと比較できないため
+    let index_path = Path::new("./.smallgit/index");
+    if !index_path.exists() {
+      return Err("index file not found".to_string());
+    }
+    let mut index_file = File::create(index_path).unwrap();
+    for path in self.get_paths_file().iter() {
+      index_file.write(&format!("{}\n", path).as_bytes()).unwrap();
+    }
+    return Ok(());
+  }
 
+  fn create_objects(&self) -> Result<(), String> {
+    let objects_path = "./.smallgit/objects";
+    let index_path = Path::new("./.smallgit/index");
+    if !Path::new(objects_path).exists() {
+      return Err("objects dir is not found".to_string());
+    }
+
+    match File::open(index_path) {
+      Ok(file) => {
+        let reader = BufReader::new(file);
+        for line in reader.lines() {
+          let line = line.unwrap();
+          let line_splits: Vec<&str> = line.split(" ").collect();
+          let add_path = Path::new(line_splits[0]);
+          if !add_path.exists() {
+            return Err("file not found error".to_string());
+          }
+
+          let content = fs::read_to_string(add_path).unwrap();
+          let format_content = format!("blob {}\0{}", content.as_bytes().len(), content);
+
+          let mut hasher = Sha1::new();
+          hasher.input_str(&format_content);
+          let hex = hasher.result_str();
+
+          let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+          e.write_all(format_content.as_bytes()).unwrap();
+          match e.finish() {
+            Ok(byte) => {
+              let objects_path_format = format!("{}/{}", objects_path, hex);
+              let mut file = File::create(objects_path_format).unwrap();
+              file.write_all(&byte).unwrap();
+            }
+            Err(_) => return Err("zlib encode error".to_string()),
+          }
+        }
+      }
+
+      Err(_) => {
+        return Err("index file is not found".to_string());
+      }
+    }
     return Ok(());
   }
 
@@ -137,6 +197,10 @@ impl AddFile {
 
   pub fn set_paths_file(&mut self, file: &Vec<String>) {
     self.paths_file = file.clone();
+  }
+
+  pub fn get_paths_file(&self) -> &Vec<String> {
+    return &self.paths_file;
   }
 
   pub fn push_ignore(&mut self, ignore_path: &str) {
