6edfb6e51fb4057b3858217da57afb882c553852
garebare <garedaiki@outlook.jp>
2020-10-15 01:21:25 +0900

    fix

--- src/commit/commit_file.rs
+++ src/commit/commit_file.rs
@@ -1,6 +1,10 @@
 use super::index_readed::IndexReaded;
+use crypto::digest::Digest;
+use crypto::sha1::Sha1;
+use flate2::write::ZlibEncoder;
+use flate2::Compression;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
 use std::path::Path;
 
 #[derive(Clone, Debug)]
@@ -72,7 +76,6 @@ impl CommitObject {
         let hex = &paths.clone().get_hex();
         let mut path_dir = paths_dir.to_string();
         path_dir.remove(0);
-
         let paths_dir_split: Vec<&str> = path_dir.split("/").collect();
         let path_split: Vec<&str> = path.split("/").collect();
         let dir_len = paths_dir_split.len() - 1;
@@ -88,21 +91,10 @@ impl CommitObject {
           break;
         }
 
-        if dir_len == path_len {
-          let take_object = TakeObject {
-            file: path.to_string(),
-            dir: path_dir.to_string(),
-            hex: hex.to_string(),
-          };
-          self.take_object.push(take_object);
-          break;
-        }
-
         for (index, dir) in paths_dir_split.iter().enumerate() {
           if !(dir == &path_split[index]) && !(dir_len == path_len) {
             break;
           }
-
           if dir_len == index {
             let take_object = TakeObject {
               file: path.to_string(),
@@ -117,7 +109,7 @@ impl CommitObject {
     }
   }
 
-  fn create_tree(&mut self) {
+  fn create_tree(&mut self) -> Result<(), String> {
     //使うパスをすべて格納
     let mut paths_dir: Vec<&str> = Vec::new();
     for path in self.take_object.iter() {
@@ -125,11 +117,10 @@ impl CommitObject {
     }
     paths_dir.sort();
     paths_dir.dedup();
-
     for paths in paths_dir.iter() {
       let tree_object = TreeObject {
         path_tree: paths.to_string(),
-        inner: "".to_string(),
+        inner: "\0".to_string(),
         hex: "".to_string(),
       };
       self.tree_object.push(tree_object);
@@ -139,18 +130,28 @@ impl CommitObject {
       let dir = &object.dir;
       let file = &object.file;
       let hex = &object.hex;
-
-      for index in 0..paths_dir.len() {
+      for index in 0..self.tree_object.len() {
         let paths_tree = &self.tree_object[index].path_tree;
         if paths_tree == dir {
           let inner = &self.tree_object[index].inner;
-          let file :Vec<&str> = file.split("/").collect();
+          let file: Vec<&str> = file.split("/").collect();
           let file_name = file[file.len() - 1];
-          self.tree_object[index].inner = format!("{} brob {} {}\n", inner, file_name ,hex);
+          self.tree_object[index].inner = format!("{}brob {} {}\n", inner, file_name, hex);
         }
       }
     }
 
+    for index in (0..self.tree_object.len()).rev() {
+      let inner = &self.tree_object[index].inner;
+      let format_inner = format!("tree {}{}", inner.to_string().as_bytes().len(), inner);
+      let mut hasher = Sha1::new();
+      hasher.input_str(&format_inner);
+      let hex = hasher.result_str();
+      self.tree_object[index].inner = format_inner;
+      self.tree_object[index].hex = hex;
+    }
+
     println!("{:?}", self.tree_object);
+    return Ok(());
   }
 }
